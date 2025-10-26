25d1c6a0fd889f0297979d632a5f45be1f8c9828
benasbart <bbart23@student.sdu.dk>
2025-10-26 22:59:29 +0100

    ensure that comma is used for parsing decimal points

--- AutoInsert.Core/Services/ThreadHoleReadingService.cs
+++ AutoInsert.Core/Services/ThreadHoleReadingService.cs
@@ -1,27 +1,28 @@
+using System.Globalization;
 using AutoInsert.Shared.Models;
 
 namespace AutoInsert.Core.Services
 {
     public class ThreadHoleReadingService
-    {   
-        public static List<ThreadHole> ReadThreadHolesFromFile(string filePath, string seperator = ";")
+    {
+        private static readonly CultureInfo Culture = new("da-DK"); // Danish culture uses comma as decimal separator
+        public static List<ThreadHole> ReadThreadHolesFromFile(string filePath, string separator = ";")
         {
             var threadHoles = new List<ThreadHole>();
-
             var lines = File.ReadAllLines(filePath);
             foreach (var line in lines)
             {
-                var parts = line.Split(seperator);
+                var parts = line.Split(separator);
                 if (parts.Length == 7)
                 {
                     var threadHole = new ThreadHole(
                         parts[0],
-                        double.Parse(parts[1]),
-                        double.Parse(parts[2]),
-                        double.Parse(parts[3]),
-                        double.Parse(parts[4]),
-                        double.Parse(parts[5]),
-                        double.Parse(parts[6])
+                        double.Parse(parts[1], Culture),
+                        double.Parse(parts[2], Culture),
+                        double.Parse(parts[3], Culture),
+                        double.Parse(parts[4], Culture),
+                        double.Parse(parts[5], Culture),
+                        double.Parse(parts[6], Culture)
                     );
                     threadHoles.Add(threadHole);
                 }
